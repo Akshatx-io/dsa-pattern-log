779260e487dbb6c91bb4226ad64955bc0468e405
Akshat Chauhan <cakshatn11@gmail.com>
2026-08-21 23:58:43 +0530

    Initial commit: DSA pattern log

---
+++ cpp/0-freopenCheck.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        if (freopen("input.txt", "r", stdin) == NULL) {
+            perror("freopen input failed");
+            return 1;
+        }
+        if (freopen("output.txt", "w", stdout) == NULL) {
+            perror("freopen output failed");
+            return 1;
+        }
+    #endif
+
+    cout << "Testing output redirection!\n";
+    return 0;
+}
---
+++ cpp/1-hello.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main()
+{	
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif	
+
+    string str;
+    getline(cin,str);
+    cout<<"Hello "<<str<<endl;
+
+    return 0;
+}
